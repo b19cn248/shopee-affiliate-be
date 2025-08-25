76be5c806aa84ec9e1637545ae9f7f848d0356c8
hieunguyen <hieunguyen@vnpt.vn>
2025-08-25 11:07:09 +0700

    test trigger github webhook

--- src/main/java/com/ptit/shopeeaffiliatebe/controller/TestController.java
+++ src/main/java/com/ptit/shopeeaffiliatebe/controller/TestController.java
@@ -2,10 +2,7 @@ package com.ptit.shopeeaffiliatebe.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/v1/tests")
@@ -18,7 +15,7 @@ public class TestController {
         return "Hello World!";
     }
 
-    @PostMapping("/dm/git")
+    @PutMapping("/dm/git")
     public String post() {
         return "Hello World!";
     }
