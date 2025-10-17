995be4a9370b74964b7a18f2ea14aa272036d6fd
hieunguyen <hieunguyen@vnpt.vn>
2025-10-17 09:55:47 +0700

    build fail lam con cho

--- src/main/java/com/ptit/shopeeaffiliatebe/controller/VoucherController.java
+++ src/main/java/com/ptit/shopeeaffiliatebe/controller/VoucherController.java
@@ -24,6 +24,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/v1/vouchers")
 @RequiredArgsConstructor
 @Slf4j
+@Tag(name = "Voucher Management", description = "APIs for managing vouchers")
 public class VoucherController {
     
     private final VoucherService voucherService;
